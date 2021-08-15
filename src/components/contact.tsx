2c788c8201705c501a7bcf1d5650938511d818a7
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-15 15:37:45 -0700

    update contact form styling and code

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { useState } from 'react';
+import './contact.css';
+// import GoogleMapReact from 'google-map-react';
 
 
 
@@ -14,43 +15,118 @@ type ContactProps = {
 
 
 const Contact: React.FC<ContactProps> = ({name, email, lat, lon, city, state}: ContactProps) => {
-    return (
-        <article className="portfolio__contact">
-            <h2>Contact</h2>
-            <p>
-                Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
-            </p>
-            <section>
-                <section className="portfolio__contact__contact-methods-container">
-                    <section className="portfolio__contact__contact-methods-container__contact-methods">
-                        <div className="portfolio__contact__contact-methods-container__contact-methods__name">
-                            <h3>
-                                Name
-                            </h3>
-                            <p>{name}</p>
-                        </div>
-                        <div className="portfolio__contact__contact-methods-container__contact-methods__email">
-                            <h3>
-                                Email
-                            </h3>
-                            <p>{email}</p>                            
-                        </div>
-                        <section className="contact-map">
-                            <GoogleMapReact
-                                bootstrapURLKeys={{ key: "" }}
-                                defaultCenter={{lat: 41.8781, lng: -87.6298}}
-                                defaultZoom={30}
-                            >
-                            </GoogleMapReact>
-                        </section>
-                    </section>
-                </section>
-                <section className="portfolio__contact__contact-form-container">
-                    <form className="portfolio__sidebar__contact-form-container__form">
+    const [formData, setformData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+    });
 
+    const updateField = (event: React.FormEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        event.preventDefault();
+
+
+        const newData = {
+            ...formData,
+        }
+        switch (event.currentTarget.name) {
+            case 'name': 
+            case 'email':
+            case 'subject':
+            case 'message': 
+                newData[event.currentTarget.name] = event.currentTarget.value;
+        }
+
+        setformData(newData);
+    }
+
+    return (
+        <article className="portfolio__contact container">
+            <div className="row">
+                <div className="section-title md-12 mb-5">
+                <h2>Contact</h2>
+                <p>
+                    Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
+                </p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-5 col-md-6 col-sm-12 d-flex align-items-stretch">
+                    <section className="col-lg-3 p-2">
+                        <p>Map data goes here</p>
+                    </section>
+                </div>
+                <section className="portfolio__contact__contact__form_block col-lg-6 col-md-6 d-flex shadow-lg p-3 mb-5 bg-white rounded">
+                    <form className="portfolio__contact__form container">
+                    <div className="row d-flex">
+                        <div className="form-group col-md-6 col-sm-12 p-2">
+                            <label htmlFor="name">Your Name</label>
+                            <input 
+                                type="text" 
+                                name="name" 
+                                className="form-control" 
+                                id="name" 
+                                data-rule="minlen:4" 
+                                data-msg="Please enter at least 4 chars" 
+                                onChange={updateField}
+                                value={formData.name}
+                            />
+                            <div className="validate"></div>
+                        </div>
+                        <div className="form-group col-md-6 col-sm-12 p-2">
+                        <label htmlFor="name">Your Email</label>
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            name="email" 
+                            id="email" 
+                            data-rule="email" 
+                            data-msg="Please enter a valid email" 
+                            onChange={updateField}
+                            value={formData.email}
+                        />
+                        <div className="validate"></div>
+                        </div>
+                    </div>
+                    <div className="row d-flex">
+                        <div className="form-group col-md-12 col-sm-12">
+                            <label htmlFor="subject">Subject</label>
+                            <input 
+                                type="text" 
+                                name="subject" 
+                                className="form-control" 
+                                id="subject" 
+                                data-rule="minlen:4" 
+                                onChange={updateField}
+                                value={formData.subject}
+                            />
+                            <div className="validate"></div>
+                        </div>
+                    </div>
+                    <div className="row col-medium-12 d-flex align-items-stretch">
+                        <div className="form-group col-md-12">
+                            <label htmlFor="name">Message</label>
+                            <textarea 
+                                className="form-control" 
+                                name="message" 
+                                rows={10} 
+                                data-rule="required" 
+                                data-msg="Please write something for us"
+                                onChange={updateField}
+                                value={formData.message}
+                            ></textarea>
+                            <div className="validate"></div>
+                        </div>
+
+                    </div>
+                    <div className="form-row col-medium-12 d-flex align-items-stretch pt-5">
+                        <div className="form-group col-md-12">
+                            <button onClick={() => {console.log('clicked')}} className="btn btn-primary btn-md">Send Email</button>
+                        </div>
+                    </div>                                
                     </form>
                 </section>
-            </section>
+            </div>
 
         </article>
     );
